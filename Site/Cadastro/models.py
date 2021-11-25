20dcb9584dfd08189f4a57e5db71d5418bde7d06
Gabriel018 <duke.comandos@gmail.com>
2021-11-25 11:06:42 -0300

    1.1

---
+++ Site/Cadastro/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200)
+    idade = models.CharField(max_length= 200)
+    genero = (('M', 'Masculino'),
+             ('F','Feminino'))
+    sexo  = models.CharField(max_length=20,choices=genero)
+
+    def __str__(self):
+        return self.nome
