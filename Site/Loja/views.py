3afbd4bf0c7c5884689f18f879a4b02437735008
Gabriel018 <duke.comandos@gmail.com>
2022-04-13 17:18:39 -0300

    produtos

--- Site/Loja/views.py
+++ Site/Loja/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import produto
 
 # Create your views here.
 
 def home(request):
 
-    return HttpResponse("Opa ")
+    """template = loader.get_template('index.html')
+    return HttpResponse(template.render())"""
+
+
+    Protudos = produto.objects.all().values()
+
+    return HttpResponse(Protudos)
