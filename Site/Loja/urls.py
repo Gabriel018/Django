fa5e81c16d663b6b71fbc34324954cb335910ff8
Gabriel018 <duke.comandos@gmail.com>
2022-04-14 16:33:31 -0300

    1

--- Site/Loja/urls.py
+++ Site/Loja/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='nome'),
+    path('', views.home, name='home'),
     path('add',views.add, name='add'),
+    path('add_record',views.add_record, name='add_record'),
     path('produtos', views.produtos, name='produtos'),
+    path('delete/<int:id>', views.delete, name='delete'),
+
 ]
--- Site/Loja/views.py
+++ Site/Loja/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import produto
 
 # Create your views here.
 
 def home(request):
-    Produtos = produto.objects.all().values()
     template = loader.get_template('index.html')
-    context = {
-        'Produtos':Produtos,
-    }
-    return HttpResponse(template.render(context,request))
-
+    return HttpResponse(template.render({},request))
 
 def produtos(request):
     template = loader.get_template('produtos.html')
-    return  HttpResponse(template.render({},request))
+    Produtos = produto.objects.all().values()
+    context = {
+        'Produtos': Produtos,
+    }
+    return  HttpResponse(template.render(context,request))
 
 def add(request):
     template  = loader.get_template('cad_prod.html')
     return HttpResponse(template.render({},request))
+
+def add_record(request):
+    x = request.POST['Desc']
+    y = request.POST['Quant']
+
+    prod = produto(Desc=x,Quant=y)
+    prod.save()
+    return HttpResponseRedirect('produtos')
+
+def delete(request, id):
+    Produtos  = produto.objects.get(id=id)
+    Produtos.delete()
+    return HttpResponse('Excluido com sucesso')
+
